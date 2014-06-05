b930530852b870989b2746b94e3661af98198001
Jack Bracken <contact@jacktrick.com>
2014-06-05 20:10:22 +0100

    Deployment

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Setup
-Make sure to set ENV vars for sending mail.
+Make sure to set ENV vars for sending mail. Ensure sendmail is installed for pony.
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server '198.199.64.172', :app, :web, :primary => true
 
 set :environment, 'production'
 set(:stage_domain) {'stanthonysrosslare.com'}
-set(:deploy_to) { "/home/#{user}/anthonys/#{stage_domain}" }
+set(:deploy_to) { "/var/www/#{stage_domain}" }
